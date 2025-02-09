659750a2bc61cc0ff4beabe3f8dcb6e7c7dd4da3
Rohan-Dighe <134312516+Rohan-Dighe@users.noreply.github.com>
2025-02-09 18:00:14 +0200

    Create JenkinsCiCdDemoApplication.java
    "Added src folder with main application file"

---
+++ src/main/java/com/example/JenkinsCiCdDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiCdDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCdDemoApplication.class, args);
+    }
+}
